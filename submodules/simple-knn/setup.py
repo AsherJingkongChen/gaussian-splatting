900956129504ab3e873512f21fed48bbf8145836
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-04-15 07:45:12 +0900

    (feat): Make one submodule a stub module

--- submodules/simple-knn/setup.py
+++ submodules/simple-knn/setup.py
@@ -9,27 +9,32 @@
 # For inquiries contact  george.drettakis@inria.fr
 #
 
+"Stub module"
+
 from setuptools import setup
-from torch.utils.cpp_extension import CUDAExtension, BuildExtension
-import os
+# from torch.utils.cpp_extension import CUDAExtension, BuildExtension
+# import os
 
-cxx_compiler_flags = []
+# cxx_compiler_flags = []
 
-if os.name == 'nt':
-    cxx_compiler_flags.append("/wd4624")
+# if os.name == 'nt':
+#     cxx_compiler_flags.append("/wd4624")
 
 setup(
     name="simple_knn",
-    ext_modules=[
-        CUDAExtension(
-            name="simple_knn._C",
-            sources=[
-            "spatial.cu", 
-            "simple_knn.cu",
-            "ext.cpp"],
-            extra_compile_args={"nvcc": ["--verbose"], "cxx": cxx_compiler_flags})
-        ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
+    version="0.0.0+stub.does.nothing"
+    # ext_modules=[
+    #     CUDAExtension(
+    #         name="simple_knn._C",
+    #         sources=[
+    #         "spatial.cu", 
+    #         "simple_knn.cu",
+    #         "ext.cpp"],
+    #         extra_compile_args={"nvcc": ["--verbose"], "cxx": cxx_compiler_flags})
+    #     ],
+    # cmdclass={
+    #     'build_ext': BuildExtension
+    # }
 )
+
+print("WARNINY")
