a9c2772a746c4636ea1fe55912918c9bb3be1290
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-04-15 20:46:14 +0900

    (adjust): Organize my script

--- scripts/bench_rendered.py
+++ scripts/bench_rendered.py
@@ -1,41 +1,27 @@
 #! /usr/bin/env python
 
-ID = "train"
-print("""\
-## Dataset: `TRAIN`
+from pathlib import Path
+
+PROJECT_ROOT = (Path(__file__) / "../../").resolve()
+
+def print_section(id: str) -> None:
+    print(f"""\
+## Dataset: `{id.upper()}`
 
 | Ground Truth | Rendered - Original | Rendered - Modified |
-|--------------|---------------------|---------------------|""")
-for i in range(0, 263, 16):
-    print(
-        f"| ![](../output/{ID}-original/train/ours_30000/gt/{i:05d}.png) "
-        f"| ![](../output/{ID}-original/train/ours_30000/renders/{i:05d}.png) "
-        f"| ![](../output/{ID}-modified/train/ours_30000/renders/{i:05d}.png) |"
-    )
-for i in range(0, 38, 4):
-    print(
-        f"| ![](../output/{ID}-original/test/ours_30000/gt/{i:05d}.png) "
-        f"| ![](../output/{ID}-original/test/ours_30000/renders/{i:05d}.png) "
-        f"| ![](../output/{ID}-modified/test/ours_30000/renders/{i:05d}.png) |"
-    )
-print()
+|--------------|---------------------|---------------------|\
+""")
+    for part in ("train", "test"):
+        for img_id, _ in enumerate(PROJECT_ROOT.glob(f"output/{id}-original/{part}/ours_30000/gt/*.png")):
+            if img_id % 10 != 0:
+                continue
+            print(
+                f"| ![](../output/{id}-original/{part}/ours_30000/gt/{img_id:05d}.png) "
+                f"| ![](../output/{id}-original/{part}/ours_30000/renders/{img_id:05d}.png) "
+                f"| ![](../output/{id}-modified/{part}/ours_30000/renders/{img_id:05d}.png) |"
+            )
+    print()
 
-ID = "truck"
-print("""\
-## Dataset: `TRUCK`
-
-| Ground Truth | Rendered - Original | Rendered - Modified |
-|--------------|---------------------|---------------------|""")
-for i in range(0, 219, 16):
-    print(
-        f"| ![](../output/{ID}-original/train/ours_30000/gt/{i:05d}.png) "
-        f"| ![](../output/{ID}-original/train/ours_30000/renders/{i:05d}.png) "
-        f"| ![](../output/{ID}-modified/train/ours_30000/renders/{i:05d}.png) |"
-    )
-for i in range(0, 32, 4):
-    print(
-        f"| ![](../output/{ID}-original/test/ours_30000/gt/{i:05d}.png) "
-        f"| ![](../output/{ID}-original/test/ours_30000/renders/{i:05d}.png) "
-        f"| ![](../output/{ID}-modified/test/ours_30000/renders/{i:05d}.png) |"
-    )
-print()
+for input_dir in PROJECT_ROOT.glob("input/*"):
+    id = input_dir.name
+    print_section(id)
