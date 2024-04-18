fad55ee803a50a73e0e12ed9d45a57fc0bf0f19e
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-04-18 19:50:57 +0900

    (adjust): Modifying position lr init may degrade low quality models

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -71,7 +71,7 @@ class PipelineParams(ParamGroup):
 class OptimizationParams(ParamGroup):
     def __init__(self, parser):
         self.iterations = 30_000
-        self.position_lr_init = 0.00006
+        self.position_lr_init = 0.00016
         self.position_lr_final = 0.0000016
         self.position_lr_delay_mult = 0.01
         self.position_lr_max_steps = 30_000
